3e008818f6b2aaa6fa9d17bd9cb2233b8d74e018
Jonatas <joba.herc@gmail.com>
2023-10-16 19:10:00 -0300

    Implementacao de remove tarefa

--- lib/ListaTarefas.dart
+++ lib/ListaTarefas.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_project/AddTarefas.dart';
-import 'package:my_project/RemoverTarefas.dart';
+import 'AddTarefas.dart';
+import 'RemoverTarefas.dart';
 
 class ListaTarefas extends StatefulWidget {
   final List<String> listTarefas;
@@ -15,90 +15,102 @@ class ListaTarefasState extends State<ListaTarefas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-          title: Text(
-            'Lista de Tarefas',
-            style: TextStyle(color: Colors.white),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () async {
-                final novaTarefa = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => AddTarefas(),
-                  ),
-                );
-
-                if (novaTarefa != null) {
-                  setState(() {
-                    widget.listTarefas.add(novaTarefa);
-                  });
-                }
-              },
-              color: Colors.white,
-            ),
-            IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RemoverTarefa()),
-                );
-              },
-              color: Colors.white,
-            ),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          'Lista de Tarefas',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () async {
+              final novaTarefa = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddTarefas(),
+                ),
+              );
+
+              if (novaTarefa != null) {
+                setState(() {
+                  widget.listTarefas.add(novaTarefa);
+                });
+              }
+            },
+            color: Colors.white,
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => RemoverTarefas(
+                    listTarefas: widget.listTarefas,
+                    atualizarListaTarefas: (novaLista) {
+                      setState(() {
+                        widget.listTarefas.clear();
+                        widget.listTarefas.addAll(novaLista);
+                      });
+                    },
+                  ),
+                ),
+              );
+            },
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
                 begin: Alignment.bottomCenter,
                 end: Alignment.topCenter,
                 colors: [Colors.purple, Colors.black],
-              )),
-            ),
-            Center(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 30.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30.0),
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 3.0,
-                  ),
-                ),
-                child: widget.listTarefas.isEmpty
-                    ? Text(
-                        'Nenhuma tarefa na lista',
-                        style: TextStyle(color: Colors.white),
-                      )
-                    : ListView.builder(
-                        itemCount: widget.listTarefas.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: EdgeInsets.symmetric(vertical: 10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              border: Border.all(
-                                color: Colors.purple,
-                                width: 3,
-                              ),
-                            ),
-                            child: ListTile(
-                              title: Text(widget.listTarefas[index]),
-                              textColor: Colors.white,
-                            ),
-                          );
-                        },
-                      ),
               ),
             ),
-          ],
-        ));
+          ),
+          Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+                border: Border.all(
+                  color: Colors.purple,
+                  width: 3.0,
+                ),
+              ),
+              child: widget.listTarefas.isEmpty
+                  ? Text(
+                      'Nenhuma tarefa na lista',
+                      style: TextStyle(color: Colors.white),
+                    )
+                  : ListView.builder(
+                      itemCount: widget.listTarefas.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: Colors.purple,
+                              width: 3,
+                            ),
+                          ),
+                          child: ListTile(
+                            title: Text(widget.listTarefas[index]),
+                            textColor: Colors.white,
+                          ),
+                        );
+                      },
+                    ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
