7189e1e8199c7d2b57a9152df696cbdb49a68aec
Jonatas <joba.herc@gmail.com>
2023-10-17 17:22:14 -0300

    Calendario

---
+++ lib/calendario1.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/ListaTarefas.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendario extends StatefulWidget {
+  @override
+  _CalendarioState createState() => _CalendarioState();
+}
+
+class _CalendarioState extends State<Calendario> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          'Calendário',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2021, 1, 1),
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ListaTarefas(
+                selectedDay: selectedDay,
+                listTarefas: [],
+              ),
+            ),
+          );
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.arrow_back),
+      ),
+    );
+  }
+}
