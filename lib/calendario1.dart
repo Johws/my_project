d3c8514f815d17744f06f5416d041703b8707012
Jonatas <joba.herc@gmail.com>
2023-10-17 18:20:13 -0300

    Implementacao do calendario

--- lib/calendario1.dart
+++ lib/calendario1.dart
@@ -22,44 +22,75 @@ class _CalendarioState extends State<Calendario> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: TableCalendar(
-        firstDay: DateTime.utc(2021, 1, 1),
-        lastDay: DateTime.utc(2030, 12, 31),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        selectedDayPredicate: (day) {
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          setState(() {
-            _selectedDay = selectedDay;
-          });
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ListaTarefas(
-                selectedDay: selectedDay,
-                listTarefas: [],
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+                colors: [Colors.purple, Colors.black],
               ),
             ),
-          );
-        },
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        onPageChanged: (focusedDay) {
-          _focusedDay = focusedDay;
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Icon(Icons.arrow_back),
+          ),
+          TableCalendar(
+            calendarStyle: CalendarStyle(
+              defaultTextStyle: TextStyle(color: Colors.white),
+              selectedDecoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.deepPurple,
+              ),
+              selectedTextStyle: TextStyle(color: Colors.white),
+            ),
+            headerStyle: HeaderStyle(
+              titleTextStyle: TextStyle(
+                  color: Colors
+                      .white), // Define o estilo do texto do título como branco
+              formatButtonTextStyle: TextStyle(
+                  color: Colors
+                      .white), // Define o estilo do texto do botão de formato como branco
+            ),
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: TextStyle(
+                  color: Colors
+                      .white), // Define o estilo do texto do dia da semana como branco
+              weekendStyle: TextStyle(
+                  color: Colors
+                      .white), // Define o estilo do texto do fim de semana como branco
+            ),
+            firstDay: DateTime.utc(2021, 1, 1),
+            lastDay: DateTime.utc(2030, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+              });
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ListaTarefas(
+                    selectedDay: selectedDay,
+                    listTarefas: [],
+                  ),
+                ),
+              );
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+        ],
       ),
     );
   }
